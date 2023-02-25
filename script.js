62c1b9080827feb82d07e45a96f596535c36632d
Reis <aglemar.reis@gmail.com>
2023-02-25 13:18:14 -0300

    firstCommit

---
+++ script.js
@@ -0,0 +1,9 @@
+function currentTime() {
+    let message = document.querySelector(".secondTitle")
+    let time = new Date()
+    let hour = time.getHours()
+    let minute = time.getMinutes()
+
+    message.innerHTML = (`Time ${hour}:${minute}`)
+    
+}
